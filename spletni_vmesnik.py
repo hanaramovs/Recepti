a55f32296f57be3a8b289fb0dc5dad061eefe055
HANA RAMOVS <hanna.ramovs@gmail.com>
2022-08-31 22:21:31 +0200

    poporava html - obrazci

--- spletni_vmesnik.py
+++ spletni_vmesnik.py
@@ -4,11 +4,23 @@ import model
 
 @bottle.get('/')
 def index():
-    return bottle.template("views/base.html")
+    return bottle.template(
+        "views/base.tpl",
+        ingredient_name = model.Ingredient.name,
+        amount = model.Ingredient.amount,
+        unit = model.Ingredient.unit,
+        )
 
-@bottle.get('/kuhanje')
+
+
+
+@bottle.get('/cookbook')
 def index():
-    return bottle.template("views/kuhanje.html")    
+    return bottle.template("views/cookbook.tpl")    
+
+@bottle.get('/recepie')
+def index():
+    return bottle.template("views/recepie.tpl")    
 
 
 #to mora biti na dnu datoteke 
